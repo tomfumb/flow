4a8b4c4287d619a12d5e3633801ad0f3feee3cad
tomfumb <thomaschristian@gmail.com>
2013-11-06 00:49:29 -0800

    Removed unnecessary Backbone.Model extension

--- js/MainView.js
+++ js/MainView.js
@@ -1,4 +1,4 @@
 Flow.MainView = Backbone.View.extend({
 	
-	
+	// register for QuestionManager's reset event. How??? The event binding will probably have to come from flow-init to avoid overriding the constructor
 });
--- js/flow-init.js
+++ js/flow-init.js
@@ -5,12 +5,17 @@ $(function() {
 		return;
 	}
 	
-	var mainView = new Flow.MainView({
-		model: new Flow.CombinedModel(Flow.OutcomeManager, Flow.QuestionManager, Flow.AnswerCollection)
+	var combinedModel = new Backbone.Model();
+	combinedModel.set({
+		Outcomes: Flow.OutcomeManager,
+		Questions: Flow.QuestionManager,
+		Answers: Flow.AnswerCollection
 	});
+	
+	new Flow.MainView({model: combinedModel});
+	// now need to register for change event on combinedModel's QuestoinManager collection. How to do this?
+	// once this is registered its firing will indicate that the app is loaded and ready. However as no ajax calls are involved we could just explicitly fire an event at the bottom of this function - all the same. Event may need to be fired from combinedModel in order for mainView to pick it up
 	
 	Flow.OutcomeManager.reset(Flow.config);
 	Flow.QuestionManager.reset(Flow.config, Flow.AnswerCollection, Flow.OutcomeManager);
-	
-	debugger;
 });
