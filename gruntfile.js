398fdc267b0ec556fee6e61fbf0efabbc72f7c7f
tc <thomaschristian@gmail.com>
2014-03-21 10:48:04 -0700

    implemented versioning through gruntfile

--- gruntfile.js
+++ gruntfile.js
@@ -19,7 +19,7 @@ module.exports = function(grunt) {
           baseUrl: "js",
           mainConfigFile: "js/app.js",
           name: "app",
-          out: "js/dist/<%= pkg.name %>-<%= pkg.version %>.js",
+          out: "js/dist/<%= pkg.name %>.js",
           optimize: "none"
         }
       },
@@ -27,7 +27,7 @@ module.exports = function(grunt) {
         options: {
           optimizeCss: 'standard',
           cssIn: 'css/src/ccij.css',
-          out: 'css/dist/<%= pkg.name %>-<%= pkg.version %>.css'
+          out: 'css/dist/<%= pkg.name %>.css'
         }
       }
     },
@@ -37,16 +37,28 @@ module.exports = function(grunt) {
         "index.htm"
         /* ignoring template .html files as lint tool cannot handle snippets without body tag or underscore _.template variables e.g. <%= ...  %>  */
       ]
+    },
+    replace: {
+      urlVersion: {
+        src: ['index.htm', 'css/**/*.css', '!css/lib/*'],
+        overwrite: true,
+        replacements: [{
+          from: /\flow\-v=\d\.\d\.\d/ig,
+          to: 'flow-v=<%= pkg.version %>'
+        }]
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-requirejs');
   grunt.loadNpmTasks('grunt-lint5');
+  grunt.loadNpmTasks('grunt-text-replace');
 
   grunt.registerTask('lint', ['jshint']);
   grunt.registerTask('r', ['requirejs']);
   grunt.registerTask('html', ['lint5']);
+  grunt.registerTask('version', ['replace']);
 
-  grunt.registerTask('default', ['jshint', 'lint5', 'requirejs']);
+  grunt.registerTask('default', ['jshint', 'lint5', 'requirejs', 'version']);
 };
