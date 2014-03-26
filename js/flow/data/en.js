9797e3c3e158474895e95b4cbf340e40e5963283
tomfumb <thomaschristian@gmail.com>
2014-03-26 07:50:39 -0700

    initial work on integrating icc

--- js/flow/data/en.js
+++ js/flow/data/en.js
@@ -133,6 +133,132 @@ define(['jquery'], function($) {
 		/* International Criminal Court */
 		selector: $('#ccij_outcome_icc'),
 		condition: function(q1) {
+			
+			var relevantCountries = [
+				{country: "Afghanistan", date: "2003/05/01"}
+				{country: "Albania", date: "2003/04/01"}
+				{country: "Andorra", date: "2002/07/01"}
+				{country: "Antigua and Barbuda", date: "2002/07/01"}
+				{country: "Argentina", date: "2002/07/01"}
+				{country: "Australia", date: "2002/09/01"}
+				{country: "Austria", date: "2002/07/01"}
+				{country: "Bangladesh", date: "2010/06/01"}
+				{country: "Barbados", date: "2003/03/01"}
+				{country: "Belgium", date: "2002/07/01"}
+				{country: "Belize", date: "2002/07/01"}
+				{country: "Benin", date: "2002/07/01"}
+				{country: "Bolivia", date: "2002/09/01"}
+				{country: "Bosnia and Herzegovina", date: "2002/07/01"}
+				{country: "Botswana", date: "2002/07/01"}
+				{country: "Brazil", date: "2002/09/01"}
+				{country: "Bulgaria", date: "2002/07/01"}
+				{country: "Burkina Faso", date: "2004/07/01"}
+				{country: "Burundi", date: "2004/12/01"}
+				{country: "Cambodia", date: "2002/07/01"}
+				{country: "Canada", date: "2002/07/01"}
+				{country: "Cape Verde", date: "2012/01/01"}
+				{country: "Central African Republic", date: "2002/07/01"}
+				{country: "Chad", date: "2007/01/01"}
+				{country: "Chile", date: "2009/09/01"}
+				{country: "Colombia", date: ["2002/11/01", "2009/11/01"]}
+				{country: "Comoros", date: "2006/11/01"}
+				{country: "Congo, Democratic Republic of the", date: "2002/07/01"}
+				{country: "Congo, Republic of the", date: "2004/08/01"}
+				{country: "Cook Islands", date: "2008/10/01"}
+				{country: "Costa Rica", date: "2002/07/01"}
+				{country: "Cote d'Ivoire", date: "2013/05/01"}
+				{country: "Croatia", date: "2002/07/01"}
+				{country: "Cyprus", date: "2002/07/01"}
+				{country: "Czech Republic", date: "2009/10/01"}
+				{country: "Denmark", date: "2002/07/01"}
+				{country: "Djibouti", date: "2003/02/01"}
+				{country: "Dominica", date: "2002/07/01"}
+				{country: "Dominican Republic", date: "2005/08/01"}
+				{country: "Ecuador", date: "2002/07/01"}
+				{country: "Estonia", date: "2002/07/01"}
+				{country: "Fiji", date: "2002/07/01"}
+				{country: "Finland", date: "2002/07/01"}
+				{country: "France", date: "2002/07/01"}
+				{country: "Gabon", date: "2002/07/01"}
+				{country: "Gambia, The", date: "2002/09/01"}
+				{country: "Georgia", date: "2003/12/01"}
+				{country: "Germany", date: "2002/07/01"}
+				{country: "Ghana", date: "2002/07/01"}
+				{country: "Greece", date: "2002/08/01"}
+				{country: "Grenada", date: "2011/08/01"}
+				{country: "Guatemala", date: "2012/07/01"}
+				{country: "Guinea", date: "2003/10/01"}
+				{country: "Guyana", date: "2004/12/01"}
+				{country: "Honduras", date: "2002/09/01"}
+				{country: "Hungary", date: "2002/07/01"}
+				{country: "Iceland", date: "2002/07/01"}
+				{country: "Ireland", date: "2002/07/01"}
+				{country: "Italy", date: "2002/07/01"}
+				{country: "Japan", date: "2007/10/01"}
+				{country: "Jordan", date: "2002/07/01"}
+				{country: "Kenya", date: "2005/06/01"}
+				{country: "Latvia", date: "2002/09/01"}
+				{country: "Lesotho", date: "2002/07/01"}
+				{country: "Liberia", date: "2004/12/01"}
+				{country: "Liechtenstein", date: "2002/07/01"}
+				{country: "Lithuania", date: "2003/08/01"}
+				{country: "Luxembourg", date: "2002/07/01"}
+				{country: "Macedonia", date: "2002/07/01"}
+				{country: "Madagascar", date: "2008/06/01"}
+				{country: "Malawi", date: "2002/12/01"}
+				{country: "Maldives", date: "2011/12/01"}
+				{country: "Mali", date: "2002/07/01"}
+				{country: "Malta", date: "2003/02/01"}
+				{country: "Marshall Islands", date: "2002/07/01"}
+				{country: "Mauritius", date: "2002/07/01"}
+				{country: "Mexico", date: "2006/01/01"}
+				{country: "Moldova", date: "2011/01/01"}
+				{country: "Mongolia", date: "2002/07/01"}
+				{country: "Montenegro", date: "2007/01/01"}
+				{country: "Namibia", date: "2002/09/01"}
+				{country: "Nauru", date: "2002/07/01"}
+				{country: "Netherlands", date: "2002/07/01"}
+				{country: "New Zealand", date: "2002/07/01"}
+				{country: "Niger", date: "2002/07/01"}
+				{country: "Nigeria", date: "2002/07/01"}
+				{country: "Norway", date: "2002/07/01"}
+				{country: "Panama", date: "2002/07/01"}
+				{country: "Paraguay", date: "2002/07/01"}
+				{country: "Peru", date: "2002/07/01"}
+				{country: "Philippines", date: "2011/11/01"}
+				{country: "Poland", date: "2002/07/01"}
+				{country: "Portugal", date: "2002/07/01"}
+				{country: "Romania", date: "2002/07/01"}
+				{country: "Saint Kitts and Nevis", date: "2006/11/01"}
+				{country: "Saint Lucia", date: "2010/11/01"}
+				{country: "Saint Vincent and the Grenadines", date: "2003/03/01"}
+				{country: "Samoa", date: "2002/12/01"}
+				{country: "San Marino", date: "2002/07/01"}
+				{country: "Senegal", date: "2002/07/01"}
+				{country: "Serbia", date: "2002/07/01"}
+				{country: "Seychelles", date: "2010/11/01"}
+				{country: "Sierra Leone", date: "2002/07/01"}
+				{country: "Slovakia", date: "2002/07/01"}
+				{country: "Slovenia", date: "2002/07/01"}
+				{country: "South Africa", date: "2002/07/01"}
+				{country: "South Korea", date: "2003/02/01"}
+				{country: "Spain", date: "2002/07/01"}
+				{country: "Suriname", date: "2008/10/01"}
+				{country: "Sweden", date: "2002/07/01"}
+				{country: "Switzerland", date: "2002/07/01"}
+				{country: "Tajikistan", date: "2002/07/01"}
+				{country: "Tanzania", date: "2002/11/01"}
+				{country: "Timor-Leste", date: "2002/12/01"}
+				{country: "Trinidad and Tobago", date: "2002/07/01"}
+				{country: "Tunisia", date: "2011/09/01"}
+				{country: "Uganda", date: "2002/09/01"}
+				{country: "United Kingdom", date: "2002/07/01"}
+				{country: "Uruguay", date: "2002/09/01"}
+				{country: "Vanuatu", date: "2012/03/01"}
+				{country: "Venezuela", date: "2002/07/01"}
+				{country: "Zambia", date: "2003/02/01"}
+			];
+			
 			return true;
 		}
 	},{
