a01c153f4efa1bdc286f3af36347a140e8b74177
tomfumb <thomaschristian@gmail.com>
2014-04-23 11:56:02 -0700

    integrated CRC

--- js/flow/data/un-crc.js
+++ js/flow/data/un-crc.js
@@ -2,7 +2,95 @@ define(['jquery'], function($) {
 	return {
 		/* U.N. Committee on the Rights of the Child (CRC) */
 		selector: $('#ccij_outcome_un_crc'),
-		condition: function(q1) {
+		condition: function(q1, q2a, q2b, q3, q6, q10, q14a, q14b, q14c, q15) {
+			
+			var relevantCountries = [
+				{country: "Albania", date: "2014/04/14"},
+				{country: "Bolivia", date: "2014/04/14"},
+				{country: "Costa Rica", date: "2014/04/14"},
+				{country: "Gabon", date: "2014/04/14"},
+				{country: "Germany", date: "2014/04/14"},
+				{country: "Montenegro", date: "2014/04/14"},
+				{country: "Portugal", date: "2014/04/14"},
+				{country: "Slovakia", date: "2014/04/14"},
+				{country: "Spain", date: "2014/04/14"},
+				{country: "Thailand", date: "2014/04/14"},
+			];
+			
+			var primaryAbuse = 'Abuse against a child under the age of 18';
+			
+			var relevantAbuses = ['Beating', 'Bodily mutilation', 'Burning', 'Burning of houses', 'Death threats', 'Denial of fair trial', 'Deprivation of medical care', 'Destruction or serious damage to property', 'Disappearance', 'Electric shock', 'Enslavement', 'Forced abortion', 'Forced displacement', 'Forced nudity', 'Forced sterilization', 'Forced stress positions', 'Forced to watch abuse of other prisoners', 'Forcing a prisoner to perform military service', 'Incommunicado detention', 'Kicking', 'Kidnapping/disappearance', 'Killing', 'Mock execution', 'Persecutions on political, racial, or religious grounds', 'Poisoning of water or food supplies', 'Prolonged exposure to extreme cold or heat', 'Prolonged food/water deprivation', 'Prolonged sleep deprivation', 'Punching', 'Rape or other sexual assault', 'Religious persecution', 'Serious mental harm to a person based on race, ethnicity, religion or nationality', 'Severe mental suffering', 'Solitary confinement', 'Stealing children', 'Suffocation', 'Waterboarding'];
+			
+			var relevantAbusers = ["Soldier in government's army", 'Police officer', 'Other government official'];
+			
+			var relevantActionOutcomes = ['Investigation or prosecution still ongoing', 'A court held someone responsible', 'Someone was put on trial but was found not guilty'];
+			
+			var proceed;
+			
+			// check relevant country(ies) selected
+			proceed = false;
+			if(q1.isNotAnswered() || q1.hasCountry(relevantCountries)) {
+				proceed = true;
+			}
+			if(q2a.hasAnswer('Yes') && (q2b.hasCountry(relevantCountries))) {
+				proceed = true;
+			}
+			
+			// exit if no relevant country(ies)
+			if(!proceed) {
+				return false;
+			}
+			
+			// check abuse date / end date against selected country(ies)
+			proceed = false;
+			var q1Dates = q1.relevantDatesForSelectedCountry(relevantCountries), q2bDates = q2b.relevantDatesForSelectedCountry(relevantCountries);
+			if (
+				q3.isNotAnswered() ||
+				(q1.isAnswered() && q1Dates && q3.isAfterOrOnDate(q1Dates)) ||
+				(q2b.isAnswered() && q2bDates && q3.isAfterOrOnDate(q2bDates))) {
+				proceed = true;
+			}
+			
+			// exit if country(ies) not covered by mechanism on the entered date
+			if(!proceed) {
+				return false;
+			}
+			
+			// check for covered abuses, primary abuse (against a child) must be checked in combination with any of the other relevant abuses
+			proceed = (q6.isNotAnswered() || (q6.hasAnswer(primaryAbuse) && q6.hasOneOfAnswers(relevantAbuses)));
+			// exit if no relevant abuses
+			if(!proceed) {
+				return false;
+			}
+
+			// check for covered abusers
+			proceed = (q10.isUnknownOrNotAnswered() || q10.hasOneOfAnswers(relevantAbusers));
+			// exit if no relevant abusers
+			if(!proceed) {
+				return false;
+			}
+			
+			// check whether domestic remedies exhausted
+			proceed = false;
+			if(
+				(q14a.isUnknownOrNotAnswered() || q14a.hasAnswer('No')) ||
+				(q14a.hasAnswer('Yes') && q14b.isUnknownOrNotAnswered()) ||
+				(q14a.hasAnswer('Yes') && q14b.hasAnswer('Yes') && q14c.isNotAnswered()) ||
+				(q14a.hasAnswer('Yes') && q14b.hasAnswer('Yes') && q14c.doesNotHaveOneOfAnswers(relevantActionOutcomes))) {
+				proceed = true;
+			}
+			// exit if domestic remedy reached a particular outcome	
+			if(!proceed) {
+				return false;
+			}
+			
+			// check whether other international remedies sought
+			proceed = q15.doesNotHaveAnswer('Yes');
+			// exit if another remedy sought
+			if(!proceed) {
+				return false;
+			}
+			
 			return true;
 		}
 	};
